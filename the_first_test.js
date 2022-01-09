acebdded3a98717276ce8de11a1f8c1e44bea622
merlin-mallory <85780760+merlin-mallory@users.noreply.github.com>
2022-01-09 11:40:51 +0900

    js test 1 - where will it go?

---
+++ the_first_test.js
@@ -0,0 +1,25 @@
+let a = 6
+let b = 5
+console.log(a*b)
+
+// single line comment
+/* multi
+line
+comment
+ */
+
+const pi = 3.1415
+console.log(pi)
+console.log(pi.type)
+const notQuitePi = parseFloat(pi)
+console.log(notQuitePi, notQuitePi.type)
+const notQuitePiString = notQuitePi.toString()
+console.log(notQuitePiString, notQuitePiString.type)
+const evenLessPiString = (3).toString()
+console.log(evenLessPiString)
+let hello = 'Hello'.substring(1, 2.5)
+console.log(hello)
+let hello2 = 'Hello'[2]
+console.log(hello2)
+const inf_test = 100/0
+console.log(inf_test)
